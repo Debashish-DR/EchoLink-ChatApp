5ea5baafb8dea067e288f10b4043c0591003cd70
Debashish Rout <122163274+Debashish-DR@users.noreply.github.com>
2025-06-28 23:27:52 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Toaster } from "react-hot-toast";
 import "./App.css";
 import Login from "./pages/login/login.jsx";
 import SignUp from "./pages/signup/signup.jsx";
-import Home from "./pages/home/home.jsx";
+import Home from "./pages/home/Home.jsx";
 import { useAuthContext } from "./context/AuthContext.jsx";
 
 function App() {
